7ee8c08ac7984a5939d9a238079141c12c24036f
supissara-c-lab <supissara-c@rmutp.ac.th>
2026-02-01 17:48:49 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
-self.addEventListener('fetch', function(event) {
-    // base PWA 
+const CACHE_NAME = 'mask-v1';
+self.addEventListener('install', (e) => {
+  console.log('Service Worker: Installed');
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
